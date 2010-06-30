aaa0610fc18d6f35fbc96705b6145ae52dccce23
joewilliams <joe@der-dieb.local>
2010-06-30 10:23:48 -0700

    slightly better support for ec2 east/west and error logging

--- lib/gaff/ec2_api.rb
+++ lib/gaff/ec2_api.rb
@@ -7,120 +7,122 @@ class Gaff
 
       Gaff::Log.debug(hash)
       STDOUT.flush
-     
-      ec2east = Fog::AWS::EC2.new(
-        :aws_access_key_id => hash["params"]["aws_key"],
-        :aws_secret_access_key => hash["params"]["aws_key_secret"],
-        :region => "us-east-1")
       
-      ec2west = Fog::AWS::EC2.new(
-        :aws_access_key_id => hash["params"]["aws_key"],
-        :aws_secret_access_key => hash["params"]["aws_key_secret"],
-        :region => "us-west-1")
-      
-      Gaff::Log.debug(ec2east)
-      Gaff::Log.debug(ec2west)
-      STDOUT.flush
-                              
-      case hash["method"]  
-      when "attach_volume"
-        begin
-          ec2east.describe_volumes([hash["params"]["volume_id"]]).body["volumeSet"].first["availabilityZone"]
-          result = ec2east.attach_volume(
-            hash["params"]["instance_id"],
-            hash["params"]["volume_id"],
-            hash["params"]["device"])
-        rescue
-          ec2west.describe_volumes([hash["params"]["volume_id"]]).body["volumeSet"].first["availabilityZone"]
-          result = ec2west.attach_volume(
-            hash["params"]["instance_id"],
-            hash["params"]["volume_id"],
-            hash["params"]["device"])
-        end                          
-      when "create_volume"
-        if hash["params"]["availability_zone"].include? "us-east-1"
-          result = ec2east.create_volume(
-            hash["params"]["availability_zone"],
-            hash["params"]["size"].to_i,
-            hash["params"]["snapshot_id"])
-        elsif hash["params"]["availability_zone"].include? "us-west-1"
-          result = ec2west.create_volume(
-            hash["params"]["availability_zone"],
-            hash["params"]["size"].to_i,
-            hash["params"]["snapshot_id"])
-        end
-      when "delete_volume"
-        begin
-          ec2east.describe_volumes([hash["params"]["volume_id"]]).body["volumeSet"].first["availabilityZone"]
-          result = ec2east.delete_volume(hash["params"]["volume_id"])
-        rescue
-          ec2west.describe_volumes([hash["params"]["volume_id"]]).body["volumeSet"].first["availabilityZone"]
-          result = ec2west.delete_volume(hash["params"]["volume_id"])
-        end
-      when "detach_volume"
-        begin
-          ec2east.describe_volumes([hash["params"]["volume_id"]]).body["volumeSet"].first["availabilityZone"]
-          result = ec2east.detach_volume(
-            hash["params"]["volume_id"],
-            {
-              "InstanceId" => hash["params"]["instance_id"],
-              "Device" => hash["params"]["device"],
-              "Force" => hash["params"]["force"]
-            })
-        rescue
-          ec2west.describe_volumes([hash["params"]["volume_id"]]).body["volumeSet"].first["availabilityZone"]
-          result = ec2west.detach_volume(
-            hash["params"]["volume_id"],
-            {
-              "InstanceId" => hash["params"]["instance_id"],
-              "Device" => hash["params"]["device"],
-              "Force" => hash["params"]["force"]
-            })
+      begin
+        ec2east = Fog::AWS::EC2.new(
+          :aws_access_key_id => hash["params"]["aws_key"],
+          :aws_secret_access_key => hash["params"]["aws_key_secret"],
+          :region => "us-east-1")
+        
+        ec2west = Fog::AWS::EC2.new(
+          :aws_access_key_id => hash["params"]["aws_key"],
+          :aws_secret_access_key => hash["params"]["aws_key_secret"],
+          :region => "us-west-1")
+        
+        Gaff::Log.debug(ec2east)
+        Gaff::Log.debug(ec2west)
+        STDOUT.flush
+                                
+        case hash["method"]  
+        when "attach_volume"
+          if zone(hash["params"]["volume_id"]).include? "us-east-1"
+            result = ec2east.attach_volume(hash["params"]["instance_id"], hash["params"]["volume_id"], hash["params"]["device"])
+          elsif zone(hash["params"]["volume_id"]).include? "us-west-1"
+            result = ec2west.attach_volume(hash["params"]["instance_id"], hash["params"]["volume_id"], hash["params"]["device"])
+          end                          
+        when "create_volume"
+          if hash["params"]["availability_zone"].include? "us-east-1"
+            result = ec2east.create_volume(hash["params"]["availability_zone"], hash["params"]["size"].to_i, hash["params"]["snapshot_id"])
+          elsif hash["params"]["availability_zone"].include? "us-west-1"
+            result = ec2west.create_volume(hash["params"]["availability_zone"], hash["params"]["size"].to_i, hash["params"]["snapshot_id"])
           end
-      when "launch_instances"
-        if hash["params"]["availability_zone"].include? "us-east-1"
-          result = ec2east.run_instances(
-            hash["params"]["image_id"],
-            hash["params"]["count"],
-            hash["params"]["count"],
-            {
-      	      "SecurityGroup" => hash["params"]["group_ids"],
-              "KeyName" => hash["params"]["key_name"],
-              "Placement.AvailabilityZone" => hash["params"]["availability_zone"],
-              "InstanceType" => hash["params"]["instance_type"]
-            })
-        elsif hash["params"]["availability_zone"].include? "us-west-1"
-          result = ec2west.run_instances(
-            hash["params"]["image_id"],
-            hash["params"]["count"],
-            hash["params"]["count"],
-            {
-          	  "SecurityGroup" => hash["params"]["group_ids"],
-              "KeyName" => hash["params"]["key_name"],
-              "Placement.AvailabilityZone" => hash["params"]["availability_zone"],
-              "InstanceType" => hash["params"]["instance_type"]
-            })
+        when "delete_volume"
+          volume_id = hash["params"]["volume_id"]
+          if zone(volume_id).include? "us-east-1"
+            result = ec2east.delete_volume(volume_id)
+          elsif zone(volume_id).include? "us-west-1"
+            result = ec2west.delete_volume(volume_id)
+          end
+        when "detach_volume"
+          if zone(hash["params"]["volume_id"]).include? "us-east-1"
+            result = ec2east.detach_volume(
+              hash["params"]["volume_id"],
+              {
+                "InstanceId" => hash["params"]["instance_id"],
+                "Device" => hash["params"]["device"],
+                "Force" => hash["params"]["force"]
+              })
+          elsif zone(hash["params"]["volume_id"]).include? "us-west-1"
+            result = ec2west.detach_volume(
+              hash["params"]["volume_id"],
+              {
+                "InstanceId" => hash["params"]["instance_id"],
+                "Device" => hash["params"]["device"],
+                "Force" => hash["params"]["force"]
+              })
+            end
+        when "launch_instances"
+          if hash["params"]["availability_zone"].include? "us-east-1"
+            result = ec2east.run_instances(
+              hash["params"]["image_id"],
+              hash["params"]["count"],
+              hash["params"]["count"],
+              {
+    	          "SecurityGroup" => hash["params"]["group_ids"],
+                "KeyName" => hash["params"]["key_name"],
+                "Placement.AvailabilityZone" => hash["params"]["availability_zone"],
+                "InstanceType" => hash["params"]["instance_type"]
+              })
+          elsif hash["params"]["availability_zone"].include? "us-west-1"
+            result = ec2west.run_instances(
+              hash["params"]["image_id"],
+              hash["params"]["count"],
+              hash["params"]["count"],
+              {
+  	            "SecurityGroup" => hash["params"]["group_ids"],
+                "KeyName" => hash["params"]["key_name"],
+                "Placement.AvailabilityZone" => hash["params"]["availability_zone"],
+                "InstanceType" => hash["params"]["instance_type"]
+              })
+          end
+        when "reboot_instances"
+          instance_ids = hash["params"]["instance_ids"]
+          if zone(instance_ids).include? "us-east-1"
+            result = ec2east.reboot_instances(instance_ids)
+          elsif zone(instance_ids).include? "us-west-1"
+            result = ec2west.reboot_instances(instance_ids)
+          end
+        when "terminate_instances"
+          instance_ids = hash["params"]["instance_ids"]
+          if zone(instance_ids).include? "us-east-1"
+            result = ec2east.terminate_instances(instance_ids)
+          elsif zone(instance_ids).include? "us-west-1"
+            result = ec2west.terminate_instances(instance_ids)
+          end
         end
-      when "reboot_instances"
+        
+        Gaff::Log.info(result)
+        STDOUT.flush
+      rescue Exception => e
+        Gaff::Log.error(e)
+        STDOUT.flush
+      end
+    end
+    
+    def self.zone(id)
+      if id.include? "i-"
         begin
-          ec2east.describe_instances([hash["params"]["instance_ids"]]).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
-          result = ec2east.reboot_instances(hash["params"]["instance_ids"])
+          ec2east.describe_instances(id).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
         rescue
-          ec2west.describe_instances([hash["params"]["instance_ids"]]).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
-          result = ec2west.reboot_instances(hash["params"]["instance_ids"])
+          ec2west.describe_instances(id).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
         end
-      when "terminate_instances"
+      elsif id.include? "vol-"
         begin
-          ec2east.describe_instances([hash["params"]["instance_ids"]]).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
-          result = ec2east.terminate_instances(hash["params"]["instance_ids"])
+          ec2east.describe_volumes(id).body["volumeSet"].first["availabilityZone"]
         rescue
-          ec2west.describe_instances([hash["params"]["instance_ids"]]).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
-          result = ec2west.terminate_instances(hash["params"]["instance_ids"])
+          ec2west.describe_volumes(id).body["volumeSet"].first["availabilityZone"]
         end
       end
-      
-      Gaff::Log.info(result)
-      STDOUT.flush
     end
 
   end
--- lib/gaff/slicehost_api.rb
+++ lib/gaff/slicehost_api.rb
@@ -8,23 +8,28 @@ class Gaff
       Gaff::Log.debug(hash)
       STDOUT.flush
 
-      slicehost = Fog::Slicehost.new({:slicehost_password => hash["params"]["password"]})
-
-      Gaff::Log.debug(slicehost)
-      STDOUT.flush
-      
-      case hash["method"]      
-      when "create_slice"
-        result = slicehost.create_slice(
-          hash["params"]["flavor_id"].to_i,
-          hash["params"]["image_id"].to_i,
-          hash["params"]["name"])
-      when "delete_slice"
-        result = slicehost.delete_slice(hash["params"]["slice_id"].to_i)   
+      begin
+        slicehost = Fog::Slicehost.new({:slicehost_password => hash["params"]["password"]})
+  
+        Gaff::Log.debug(slicehost)
+        STDOUT.flush
+        
+        case hash["method"]      
+        when "create_slice"
+          result = slicehost.create_slice(
+            hash["params"]["flavor_id"].to_i,
+            hash["params"]["image_id"].to_i,
+            hash["params"]["name"])
+        when "delete_slice"
+          result = slicehost.delete_slice(hash["params"]["slice_id"].to_i)   
+        end
+        
+        Gaff::Log.info(result)
+        STDOUT.flush
+      rescue Exception => e
+        Gaff::Log.error(e)
+        STDOUT.flush
       end
-      
-      Gaff::Log.info(result)
-      STDOUT.flush
     end
 
   end
