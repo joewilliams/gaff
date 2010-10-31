3c61ce70e7f83374ed4bde80a23213462ec39107
joewilliams <joe@der-dieb.local>
2010-10-31 11:49:05 -0700

    ec2 apac support

--- lib/gaff/ec2_api.rb
+++ lib/gaff/ec2_api.rb
@@ -19,8 +19,14 @@ class Gaff
           :aws_secret_access_key => hash["params"]["aws_key_secret"],
           :region => "us-west-1")
         
+        @apac = Fog::AWS::EC2.new(
+          :aws_access_key_id => hash["params"]["aws_key"],
+          :aws_secret_access_key => hash["params"]["aws_key_secret"],
+          :region => "ap-southeast-1")
+        
         Gaff::Log.debug(@ec2east)
         Gaff::Log.debug(@ec2west)
+        Gaff::Log.debug(@apac)
         STDOUT.flush
                                 
         case hash["method"]  
@@ -29,12 +35,16 @@ class Gaff
             result = @ec2east.attach_volume(hash["params"]["instance_id"], hash["params"]["volume_id"], hash["params"]["device"])
           elsif zone(hash["params"]["volume_id"]).include? "us-west-1"
             result = @ec2west.attach_volume(hash["params"]["instance_id"], hash["params"]["volume_id"], hash["params"]["device"])
+          elsif zone(hash["params"]["volume_id"]).include? "ap-southeast-1"
+            result = @apac.attach_volume(hash["params"]["instance_id"], hash["params"]["volume_id"], hash["params"]["device"])
           end                          
         when "create_volume"
           if hash["params"]["availability_zone"].include? "us-east-1"
             result = @ec2east.create_volume(hash["params"]["availability_zone"], hash["params"]["size"].to_i, hash["params"]["snapshot_id"])
           elsif hash["params"]["availability_zone"].include? "us-west-1"
             result = @ec2west.create_volume(hash["params"]["availability_zone"], hash["params"]["size"].to_i, hash["params"]["snapshot_id"])
+          elsif hash["params"]["availability_zone"].include? "ap-southeast-1"
+            result = @apac.create_volume(hash["params"]["availability_zone"], hash["params"]["size"].to_i, hash["params"]["snapshot_id"])
           end
         when "delete_volume"
           volume_id = hash["params"]["volume_id"]
@@ -42,6 +52,8 @@ class Gaff
             result = @ec2east.delete_volume(volume_id)
           elsif zone(volume_id).include? "us-west-1"
             result = @ec2west.delete_volume(volume_id)
+          elsif zone(volume_id).include? "ap-southeast-1"
+            result = @apac.delete_volume(volume_id)
           end
         when "detach_volume"
           if zone(hash["params"]["volume_id"]).include? "us-east-1"
@@ -60,7 +72,15 @@ class Gaff
                 "Device" => hash["params"]["device"],
                 "Force" => hash["params"]["force"]
               })
-            end
+          elsif zone(hash["params"]["volume_id"]).include? "ap-southeast-1"
+            result = @apac.detach_volume(
+              hash["params"]["volume_id"],
+              {
+                "InstanceId" => hash["params"]["instance_id"],
+                "Device" => hash["params"]["device"],
+                "Force" => hash["params"]["force"]
+              })
+          end
         when "launch_instances"
           if hash["params"]["availability_zone"].include? "us-east-1"
             result = @ec2east.run_instances(
@@ -84,6 +104,17 @@ class Gaff
                 "Placement.AvailabilityZone" => hash["params"]["availability_zone"],
                 "InstanceType" => hash["params"]["instance_type"]
               })
+          elsif hash["params"]["availability_zone"].include? "ap-southeast-1"
+            result = @apac.run_instances(
+              hash["params"]["image_id"],
+              hash["params"]["count"],
+              hash["params"]["count"],
+              {
+    	          "SecurityGroup" => hash["params"]["group_ids"],
+                "KeyName" => hash["params"]["key_name"],
+                "Placement.AvailabilityZone" => hash["params"]["availability_zone"],
+                "InstanceType" => hash["params"]["instance_type"]
+              })
           end
         when "reboot_instances"
           instance_ids = hash["params"]["instance_ids"]
@@ -91,6 +122,8 @@ class Gaff
             result = @ec2east.reboot_instances(instance_ids)
           elsif zone(instance_ids).include? "us-west-1"
             result = @ec2west.reboot_instances(instance_ids)
+          elsif zone(instance_ids).include? "ap-southeast-1"
+            result = @apac.reboot_instances(instance_ids)
           end
         when "terminate_instances"
           instance_ids = hash["params"]["instance_ids"]
@@ -98,6 +131,8 @@ class Gaff
             result = @ec2east.terminate_instances(instance_ids)
           elsif zone(instance_ids).include? "us-west-1"
             result = @ec2west.terminate_instances(instance_ids)
+          elsif zone(instance_ids).include? "ap-southeast-1"
+            result = @apac.terminate_instances(instance_ids)
           end
         end
         
@@ -114,13 +149,21 @@ class Gaff
         begin
           @ec2east.describe_instances(id).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
         rescue
-          @ec2west.describe_instances(id).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
+          begin
+            @ec2west.describe_instances(id).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
+          rescue
+            @apac.describe_instances(id).body["reservationSet"].first["instancesSet"].first["placement"]["availabilityZone"]
+          end
         end
       elsif id.include? "vol-"
         begin
           @ec2east.describe_volumes(id).body["volumeSet"].first["availabilityZone"]
         rescue
-          @ec2west.describe_volumes(id).body["volumeSet"].first["availabilityZone"]
+          begin
+            @ec2west.describe_volumes(id).body["volumeSet"].first["availabilityZone"]
+          rescue
+            @apac.describe_volumes(id).body["volumeSet"].first["availabilityZone"]
+          end
         end
       end
     end
